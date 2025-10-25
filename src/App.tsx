d7f89e52de3dac157ae8a8733183f4e1d281c1c4
gabrielgustavoandrade <gabrielgustavoandrade@gmail.com>
2025-10-25 09:56:27 +0200

    feat: enable router future flags

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './routes/HomePage';
 import { WorkDetailPage } from './routes/WorkDetailPage';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <div className="app">
         <Routes>
           <Route path="/" element={<HomePage />} />
